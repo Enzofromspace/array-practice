732bb09219e873498dd46baeea561d2ba9d2132a
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-13 12:58:56 +0300

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function createRange(start, end){
+  const range = Array.from({length: end - start + 1}, function(item, index){
+    return index + start;
+  });
+  return range;
+}
+
+const myRange = createRange(3,7);
