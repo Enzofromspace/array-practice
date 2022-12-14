a088e74f3831938cde474084b1a249813f0b6913
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-14 12:46:40 +0300

    This is a very messy doc don't worry about it

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Array notes in HTML code</h1>`;
+
+const feedback = [
+  { comment: 'Love the burgs', rating: 4 },
+  { comment: 'Horrible Service', rating: 2 },
+  { comment: 'Smoothies are great, liked the burger too', rating: 5 },
+  { comment: 'Ambiance needs work', rating: 3 },
+  { comment: 'I DONT LIKE BURGERS', rating: 1 },
+];
 
 function createRange(start, end){
   const range = Array.from({length: end - start + 1}, function(item, index){
@@ -13,3 +21,7 @@ function createRange(start, end){
 }
 
 const myRange = createRange(3,7);
+
+const burgRating = feedback.find(rating => rating.comment.includes('burg'));
+
+console.log(burgRating);
